f0a07a12f2396e87447a0165b80dfdd6c796cd53
Srinathpuligari <srinathpuligari@gmail.com>
2025-08-15 23:51:17 +0530

    resume updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/u/srinathpuligari",
   devUsername: "srinathpuligari",
-  resume: "https://drive.google.com/file/d/1cJ42ajJAOTC95PBzs3OxNMV7uas9OIvt/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/15iJR-nYfzzrbW8S5K5oMJZxxeSLbHxHD/view?usp=sharing"
 }
